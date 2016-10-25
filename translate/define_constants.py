59017998aa950212a90df53845b88c74338822d2
JuleLaryushina <larja1994@gmail.com>
2016-10-26 00:13:00 +0300

    Delete define_constants.py

--- translate/define_constants.py
+++
@@ -1,4 +0,0 @@
-tf.app.flags.DEFINE_boolean("data_1_seq", False,
-                            "First sequence (english)")
-tf.app.flags.DEFINE_boolean("data_2_seq", False,
-                            "Second sequence (french)")
