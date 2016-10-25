3c6cac17489e7a4423c8a5eb6440594827748c01
JuleLaryushina <larja1994@gmail.com>
2016-10-25 23:48:25 +0300

    Create define_constants.py

---
+++ translate/define_constants.py
@@ -0,0 +1,4 @@
+tf.app.flags.DEFINE_boolean("data_1_seq", False,
+                            "First sequence (english)")
+tf.app.flags.DEFINE_boolean("data_2_seq", False,
+                            "Second sequence (french)")
